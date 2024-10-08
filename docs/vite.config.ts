427e97dd3b4660f63ed6d2203f60e3d6a405f85f
amauri <brown817@msu.edu>
2024-10-08 19:39:06 -0400

    update

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.PNG', '**/*.png'], // Add this line to include PNG files
   plugins: [react()],
-  assetsInclude: ['**/*.PNG', '**/*.png'] // Add this line to include PNG files
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
