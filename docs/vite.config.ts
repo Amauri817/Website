6f9ca288d6707a127437541a5c6eeabdb739b5c9
amauri <brown817@msu.edu>
2024-10-08 19:21:55 -0400

    update

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG', '**/*.png'], // Add this line to include PNG files
-  base: '/<git@github.com:Amauri817/Website.git>/',
+  assetsInclude: ['**/*.PNG', '**/*.png'] // Add this line to include PNG files
 })
