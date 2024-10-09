8b8e57be4cd00ca01859d3b494fd9d8c71c27bf6
amauri <brown817@msu.edu>
2024-10-08 20:13:32 -0400

    new

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   assetsInclude: ['**/*.PNG', '**/*.png'], // Add this line to include PNG files
   plugins: [react()],
-  server: {
-    open: true,
-  },
+  base: '/about', // Adjust this if your app is deployed in a subdirectory
 });
