64f8c54d24c6153bbb191a957b5a7eb18c1044c3
amauri <brown817@msu.edu>
2024-10-10 06:47:17 -0400

    k

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import Home from './page/home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Home from './pages/home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
+
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
+    <>
+      <Router>
+        <Routes>
+          {/* The route for the home page */}
+          <Route path="/" element={<Home />} />
+          {/* Other routes */}
+        </Routes>
+      </Router>
+    </>
   )
 }
 
