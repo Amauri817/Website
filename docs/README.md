57dcec7f6542e27c01a5b96323178af0e6b2c572
amauri <brown817@msu.edu>
2024-10-10 01:49:35 -0400

    new

--- docs/README.md
+++
@@ -1,6 +0,0 @@
-# amauribrown.com
-
-## About 
-
-This is my personal portfolio website, created using VSCode. This is used to showcase my technical skills. For
-more information you can contact me at brown817@msu.edu
--- docs/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  assetsInclude: ['**/*.PNG', '**/*.png'], // Add this line to include PNG files
-  plugins: [react()],
-  base: '/about', // Adjust this if your app is deployed in a subdirectory
-});
