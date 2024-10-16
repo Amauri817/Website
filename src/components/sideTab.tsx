a805ac7d9096c311e14733e6541942f20f1120f6
amauri <brown817@msu.edu>
2024-10-15 20:17:40 -0400

    new

--- src/components/sideTab.tsx
+++ src/components/sideTab.tsx
@@ -50,7 +50,7 @@ const SideBar: React.FC = () => {
 
       <Link to='#certifications' >
         <div className="sidebar_items">
-            Certificications
+            Certificates
         </div>
       </Link>
 
