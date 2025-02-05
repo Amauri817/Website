e53e3fa7d57834d0a6591ce4ee88107d45b1fedf
amauri <brown817@msu.edu>
2025-02-05 02:11:06 -0500

    update

--- src/components/Design/navTab.tsx
+++ src/components/Design/navTab.tsx
@@ -1,8 +1,7 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
+
 const NavTab: React.FC = () => {
 
     return (
@@ -13,7 +12,7 @@ const NavTab: React.FC = () => {
                 <Link to="/designPage"><li>Home</li></Link>
                 <Link to="/projectsContent"><li>Projects</li></Link>
                 <Link to="/aboutContent"><li>About</li></Link>
-                <Link to="https://drive.google.com/file/d/1uohXkgqjCiM6POS0Nd8vquTuMrgUBHUC/view?usp=sharing" target='blank'><li>Resume</li></Link>
+                <Link to="https://drive.google.com/file/d/1GvViKU1oNun4h8hCeKMHQzBs297nwvbW/view?usp=sharing" target='blank'><li>Resume</li></Link>
                 <Link to="/contactContent"><li>Contact</li></Link>
             </ul>
             <Link to="/designPage"><h2 className='Logo'> Amauri Brown <br/> <span>Technical Designer</span> </h2></Link>
