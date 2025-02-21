f552d3e6c649f55b945c2ba4666a8ea25d79a256
amauri <brown817@msu.edu>
2025-02-21 12:01:17 -0500

    added date

--- src/components/Programming/certificationContent.tsx
+++ src/components/Programming/certificationContent.tsx
@@ -8,6 +8,7 @@ const CertAdd: React.FC = () => {
             <div className="container">
                 <h3>Certificate</h3>
                 <h5 style={{textAlign:'center'}}>Michigan State University(Full-Stack Development)</h5>
+                <h5 style={{textAlign:'center'}}>(January 2025)</h5>
                 <h1><a href="https://drive.google.com/file/d/14TCB87Q_qodXTj-IJOpWDeXnNaU11CPp/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                 <img src={Img} alt="My Coding Certificate" /></a></h1>
             </div>
